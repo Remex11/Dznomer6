fa9dcb941de86f209c7e0bdc8dd66ad949bb5846
user <user@WIN-ALUPCFD7D1E>
2025-01-22 18:17:25 +0500

    Добавьте файлы проекта.

---
+++ Project1/Source.cpp
@@ -0,0 +1,66 @@
+Вадим, [22.01.2025 18:11]
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows = 3; 
+    int cols = 3; 
+    int array[rows][cols];
+
+    int num;
+    cout << "Введите число: ";
+    cin >> num;
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (j == 0) {
+                array[i][j] = num;
+            }
+            else {
+                array[i][j] = array[i][j - 1] * 2;
+            }
+        }
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << array[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+// 2 задание 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows = 3; 
+    int cols = 3; 
+    int array[rows][cols];
+
+    int num;
+    cout << "Введите число: ";
+    cin >> num;
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (j == 0) {
+                array[i][j] = num;
+            }
+            else {
+                array[i][j] = array[i][j - 1] + 1;
+            }
+        }
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << array[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
